0d2963a43139504cf4165c6e493424209b363b72
cxduvall <57238295+cxduvall@users.noreply.github.com>
2023-01-04 17:00:03 -0600

    gui stays on screen

--- simulate.py
+++ simulate.py
@@ -1,3 +1,11 @@
-import pybullet
+import pybullet as p
+import time
 
-print("yo")
+physicsClient = p.connect(p.GUI)
+
+for i in range(1000):
+    p.stepSimulation()
+    time.sleep(1/60)
+    print(i)
+
+p.disconnect()

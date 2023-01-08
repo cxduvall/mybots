f32951f71b1733470de0fd853c400fec44c3523e
cxduvall <57238295+cxduvall@users.noreply.github.com>
2023-01-07 18:58:51 -0600

    URDF cube

--- generate.py
+++ generate.py
@@ -1,23 +1,24 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("boxes.sdf")
-
 length = 1
 width = 1
 height = 1
 x = 0
 y = 0
 z = 0.5
+scale = 1
+
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[x-2,y+2,z] , size=[width*scale,length*scale,height*scale])
+    pyrosim.End()
+
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[x,y,z] , size=[width*scale,length*scale,height*scale])
+    pyrosim.End()
+
+Create_World()
+Create_Robot()
 
 
-for j in range(5):
-    for k in range(5):
-        scale = 1
-        z = 0.5
-        for i in range(5):
-            pyrosim.Send_Cube(name="Box" + str(i), pos=[x+j,y+k,z] , size=[width*scale,length*scale,height*scale])
-            z += scale/2
-            scale *= 0.9
-            z += scale/2
-
-pyrosim.End()

50b4001a00f9a2efdf1027a0b1ff5d8ca13ce869
cxduvall <57238295+cxduvall@users.noreply.github.com>
2023-01-07 18:46:24 -0600

    part d

--- generate.py
+++ generate.py
@@ -8,7 +8,16 @@ height = 1
 x = 0
 y = 0
 z = 0.5
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[width,length,height])
-pyrosim.Send_Cube(name="Box2", pos=[x+1,y,z+1] , size=[width,length,height])
+
+
+for j in range(5):
+    for k in range(5):
+        scale = 1
+        z = 0.5
+        for i in range(5):
+            pyrosim.Send_Cube(name="Box" + str(i), pos=[x+j,y+k,z] , size=[width*scale,length*scale,height*scale])
+            z += scale/2
+            scale *= 0.9
+            z += scale/2
 
 pyrosim.End()

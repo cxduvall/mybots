15fe78b4d5f14e2ef85ab25f6c149044c8da844a
cxduvall <57238295+cxduvall@users.noreply.github.com>
2023-01-07 19:10:36 -0600

    wrong joint

--- generate.py
+++ generate.py
@@ -16,6 +16,8 @@ def Create_World():
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
     pyrosim.Send_Cube(name="Torso", pos=[x,y,z] , size=[width*scale,length*scale,height*scale])
+    pyrosim.Send_Joint( name = "Torso_Leg" , parent= "Torso" , child = "Leg" , type = "revolute", position = [x+0.5,y,z+0.5])
+    pyrosim.Send_Cube(name="Leg", pos=[x+1,y,z+1] , size=[width*scale,length*scale,height*scale])
     pyrosim.End()
 
 Create_World()
